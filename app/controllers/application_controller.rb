5d93678eba97ce964c7b3409559a4881ee7bb45b
josegre1 <josegre1@hotmail.com>
2017-06-22 03:30:47 -0500

    API Documentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Acces-Control-Allow-Origin'] = '*'
+    headers['Acces-Control-Request-Method'] = '*'
+  end
   include SessionsHelper
 end
